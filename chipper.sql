43016f7c17ff414f2f63c2314e6dc70bc057c842
Tembi <61012608+ngotyi@users.noreply.github.com>
2020-02-19 10:54:19 +0200

    Add files via upload

--- chipper.sql
+++ chipper.sql
@@ -1,1 +1,11 @@
-SELECT * FROM mydatabase.drinks;
+/*
+-- Query: SELECT * FROM mydatabase.households
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 10:54
+*/
+INSERT INTO `` (`Product`,`Barcode`,`weight`,`scale`) VALUES ('Pads','2293856394','500','g');
+INSERT INTO `` (`Product`,`Barcode`,`weight`,`scale`) VALUES ('Moisturizer','2276517584','600','g');
+INSERT INTO `` (`Product`,`Barcode`,`weight`,`scale`) VALUES ('Facial cleanser','2840109754','300','g');
+INSERT INTO `` (`Product`,`Barcode`,`weight`,`scale`) VALUES ('Makeup','58735011054','20.0','g');
+INSERT INTO `` (`Product`,`Barcode`,`weight`,`scale`) VALUES ('Colognes','7495627313087','500','g');
