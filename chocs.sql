004ac270dc1f7b42e34f4667c4b2be363cfb2bac
Tembi <61012608+ngotyi@users.noreply.github.com>
2020-02-19 11:10:13 +0200

    Add files via upload

---
+++ chocs.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM mydatabase.chocs
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 11:10
+*/
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Mars','2293872892','19.7','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Kit Kat','2276546852','7.8','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Cadbury','6001087313087','0.002','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Lindt','2840296854','200','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Tex','2840011054','200','g');
