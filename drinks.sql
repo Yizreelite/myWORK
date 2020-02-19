20693f46996330079b7b6dec70f96c06628bd544
Tembi <61012608+ngotyi@users.noreply.github.com>
2020-02-19 11:13:00 +0200

    Add files via upload

---
+++ drinks.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM mydatabase.drinks
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 11:12
+*/
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('coca','2840011054','200','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('Schweppes','2293872892','19.7','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('score','2276546852','7.8','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('sprite','6001087313087','0.002','g');
+INSERT INTO `` (`Type`,`Barcode`,`weight`,`scale`) VALUES ('fanta','2840296854','200','g');
